cecbe8b8bde5bd33c08bab7cc2a0d91bf1052d8c
a0930014306 <loveisspacewar@gmail.com>
2022-07-14 22:01:41 +0800

    20220714

--- Web_practice_source/js/index.js
+++ Web_practice_source/js/index.js
@@ -1,1 +1,101 @@
+const $slidels = $('#SlideLs')
+const isMobile = $(window).width() <= 480
+const $nav = $('#Nav')
 
+console.log(isMobile);
+
+$('#Btn1').addClass('js-nav-btn')
+
+$('#Btn1').click(function () {
+    if ($(this).hasClass('js-nav-btn')) {
+        return
+    }
+    $(this).addClass('js-nav-btn').siblings().removeClass('js-nav-btn')
+    $slidels.css({
+        left: '0',
+        top: '0'
+    })
+    if(isMobile){
+        $nav.removeClass('js-nav')
+    }
+
+})
+$('#Btn2').click(function () {
+    if ($(this).hasClass('js-nav-btn')) {
+        return
+    }
+    $(this).addClass('js-nav-btn').siblings().removeClass('js-nav-btn')
+    $slidels.css({
+        left: '-100vw',
+        top: '0'
+    })
+    if(isMobile){
+        $nav.removeClass('js-nav')
+    }
+})
+$('#Btn3').click(function () {
+    if ($(this).hasClass('js-nav-btn')) {
+        return
+    }
+    $(this).addClass('js-nav-btn').siblings().removeClass('js-nav-btn')
+    $slidels.css({
+        left: '-200vw',
+        top: '0'
+    })
+    if(isMobile){
+        $nav.removeClass('js-nav')
+    }
+})
+$('#Btn4').click(function () {
+    if ($(this).hasClass('js-nav-btn')) {
+        return
+    }
+    $(this).addClass('js-nav-btn').siblings().removeClass('js-nav-btn')
+    $slidels.css({
+        left: '0',
+        top: '-100vh'
+    })
+    if(isMobile){
+        $nav.removeClass('js-nav')
+    }
+})
+$('#Btn5').click(function () {
+    if ($(this).hasClass('js-nav-btn')) {
+        return
+    }
+    $(this).addClass('js-nav-btn').siblings().removeClass('js-nav-btn')
+    $slidels.css({
+        left: '-100vw',
+        top: '-100vh'
+    })
+    if(isMobile){
+        $nav.removeClass('js-nav')
+    }
+})
+$('#Btn6').click(function () {
+    if ($(this).hasClass('js-nav-btn')) {
+        return
+    }
+    $(this).addClass('js-nav-btn').siblings().removeClass('js-nav-btn')
+    $slidels.css({
+        left: '-200vw',
+        top: '-100vh'
+    })
+    if(isMobile){
+        $nav.removeClass('js-nav')
+    }
+})
+
+if (isMobile) {
+    $slidels.find('.slide-item').scroll(function () {
+        if ($(this).scrollTop() == 0) {
+           $nav.removeClass('js-nav')
+        } else {
+           $nav.addClass('js-nav')
+        }
+    })
+}
+
+$('.pd-item > a').fancybox({
+    protect:true
+})
